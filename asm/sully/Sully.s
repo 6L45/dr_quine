c31d09d45b3b3eab1f5ec4f986b225af64fbf379
silas <silas@msi>
2024-11-11 23:32:49 +0100

    correction char -> 2 -> int in Sully.s

--- asm/sully/Sully.s
+++ asm/sully/Sully.s
@@ -3,14 +3,15 @@ extern dprintf
 extern system
 
 section .data
-	quine db "global main%3$cextern dprintf%3$cextern system%3$c%3$csection .data%3$c%2$cquine db %4$c%1$s%4$c, 0%3$c%2$cfilename db %4$cSully_0.s%4$c, 0%3$c%2$ccmd db %4$c%5$s%4$c, 0%3$c%2$cit db %4$c%6$c%4$c%3$c%3$csection .text%3$cmain:%3$c%2$cmovzx rax, byte [rel it]%3$c%2$ccmp rax, 0x2F%3$c%2$cje out0%3$c%3$c%2$clea r10, [rel filename]%3$c%2$cadd r10, 6%3$c%2$cmov [r10], al%3$c%3$c%2$cmov rax, 2%3$c%2$clea rdi, [rel filename]%3$c%2$cmov rsi, 0x241%3$c%2$cmov rdx, 0x180%3$c%2$csyscall%3$c%2$ccmp rax, 0%3$c%2$cjl outopen%3$c%2$cpush rax%3$c%3$c%2$cmov rdi, rax%3$c%2$clea rsi, [rel quine]%3$c%2$clea rdx, [rel quine]%3$c%2$cmov rcx, 0x09%3$c%2$cmov r8, 0x0A%3$c%2$cmov r9, 0x22%3$c%2$cmovzx r12, byte [rel it]%3$c%2$cdec r12%3$c%2$cpush r12%3$c%2$clea r12, [rel cmd]%3$c%2$cpush r12%3$c%3$c%2$ccall dprintf wrt ..plt%3$c%3$c%2$cmov rax, 3%3$c%2$cpop rdi%3$c%2$csyscall%3$c%3$c%2$clea rax, [rel cmd]%3$c%2$cmov r10b, [rel it]%3$c%2$cmov byte [rax + 34], r10b%3$c%2$cmov byte [rax + 59], r10b%3$c%2$cmov byte [rax + 72], r10b%3$c%2$cmov byte [rax + 85], r10b%3$c%3$c%2$clea rdi, [rel cmd]%3$c%3$c%2$cmov rax, rsp%3$c%2$cand rax, 0xF%3$c%2$csub rsp, rax%3$c%3$c%2$ccall system wrt ..plt%3$c%3$c%2$cout0:%3$c%2$c%2$cmov rax, 60%3$c%2$c%2$cmov rdi, 0%3$c%2$c%2$csyscall%3$c%3$c%2$cout1:%3$c%2$c%2$cmov rax, 60%3$c%2$c%2$cmov rdi, 1%3$c%2$c%2$csyscall%3$c%3$c%2$coutopen:%3$c%2$c%2$cmov rdi, rax%3$c%2$c%2$cmov rax, 60%3$c%2$c%2$csyscall%3$c%3$csection .note.GNU-stack noalloc noexec nowrite progbits%3$c", 0
+	quine db "global main%3$cextern dprintf%3$cextern system%3$c%3$csection .data%3$c%2$cquine db %4$c%1$s%4$c, 0%3$c%2$cfilename db %4$cSully_0.s%4$c, 0%3$c%2$ccmd db %4$c%5$s%4$c, 0%3$c%2$cit db %6$d%3$c%3$csection .text%3$cmain:%3$c%2$cmovsx rax, byte [rel it]%3$c%2$cadd rax, 48%3$c%2$ccmp rax, 0x2F%3$c%2$cje out0%3$c%3$c%2$clea r10, [rel filename]%3$c%2$cadd r10, 6%3$c%2$cmov [r10], al%3$c%3$c%2$cmov rax, 2%3$c%2$clea rdi, [rel filename]%3$c%2$cmov rsi, 0x241%3$c%2$cmov rdx, 0x180%3$c%2$csyscall%3$c%2$ccmp rax, 0%3$c%2$cjl outopen%3$c%2$cpush rax%3$c%3$c%2$cmov rdi, rax%3$c%2$clea rsi, [rel quine]%3$c%2$clea rdx, [rel quine]%3$c%2$cmov rcx, 0x09%3$c%2$cmov r8, 0x0A%3$c%2$cmov r9, 0x22%3$c%2$cmovzx r12, byte [rel it]%3$c%2$cdec r12%3$c%2$cpush r12%3$c%2$clea r12, [rel cmd]%3$c%2$cpush r12%3$c%3$c%2$ccall dprintf wrt ..plt%3$c%3$c%2$cmov rax, 3%3$c%2$cpop rdi%3$c%2$csyscall%3$c%3$c%2$clea rax, [rel cmd]%3$c%2$cmovzx r10, byte [rel it]%3$c%2$cadd r10, 48%3$c%2$cmov byte [rax + 34], r10b%3$c%2$cmov byte [rax + 59], r10b%3$c%2$cmov byte [rax + 72], r10b%3$c%2$cmov byte [rax + 85], r10b%3$c%3$c%2$clea rdi, [rel cmd]%3$c%3$c%2$cmov rax, rsp%3$c%2$cand rax, 0xF%3$c%2$csub rsp, rax%3$c%3$c%2$ccall system wrt ..plt%3$c%3$c%2$cout0:%3$c%2$c%2$cmov rax, 60%3$c%2$c%2$cmov rdi, 0%3$c%2$c%2$csyscall%3$c%3$c%2$cout1:%3$c%2$c%2$cmov rax, 60%3$c%2$c%2$cmov rdi, 1%3$c%2$c%2$csyscall%3$c%3$c%2$coutopen:%3$c%2$c%2$cmov rdi, rax%3$c%2$c%2$cmov rax, 60%3$c%2$c%2$csyscall%3$c%3$csection .note.GNU-stack noalloc noexec nowrite progbits%3$c", 0
 	filename db "Sully_0.s", 0
 	cmd db "/bin/bash -c 'nasm -f elf64 Sully_0.s && gcc -no-pie Sully_0.o -o Sully_0 && ./Sully_0 && rm -f *.o'", 0
-	it db "5"
+	it db 5
 
 section .text
 main:
-	movzx rax, byte [rel it]
+	movsx rax, byte [rel it]
+	add rax, 48
 	cmp rax, 0x2F
 	je out0
 
@@ -46,7 +47,8 @@ main:
 	syscall
 
 	lea rax, [rel cmd]
-	mov r10b, [rel it]
+	movzx r10, byte [rel it]
+	add r10, 48
 	mov byte [rax + 34], r10b
 	mov byte [rax + 59], r10b
 	mov byte [rax + 72], r10b
